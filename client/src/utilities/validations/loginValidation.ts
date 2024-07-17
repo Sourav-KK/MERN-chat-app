8323df3d0d7027f67db4dab8faf5b97832e69e64
Sourav <souravunofficial000@gmail.com>
2024-07-17 13:30:55 +0530

    git merge solve issure

--- client/src/utilities/validations/loginValidation.ts
+++ client/src/utilities/validations/loginValidation.ts
@@ -1,19 +1,9 @@
 import Zod from "zod";
-<<<<<<< HEAD
-=======
 import { alphaNumsSplCharRegex, emailRegex } from "../regex";
->>>>>>> 5607caf7a3795fd6618d20f5fe37795eb341a2dd
 
 export const loginFormSchema = Zod.object({
   emailORuserName:
     Zod.string()
-<<<<<<< HEAD
-      .email()
-      .min(4, "Invalid email")
-      .max(20, "Maximum 20 characters for email") ||
-    Zod.string().min(2, "min length is 2").max(1, "Max lenth 1"),
-  password: Zod.string().min(4, "min length").max(16, "MAx lenth"),
-=======
       .trim()
       .email()
       .regex(emailRegex)
@@ -30,5 +20,4 @@ export const loginFormSchema = Zod.object({
     .min(4, "Should contain 6-20 characters")
     .max(16, "Should contain 6-20 characters")
     .regex(alphaNumsSplCharRegex),
->>>>>>> 5607caf7a3795fd6618d20f5fe37795eb341a2dd
 });
