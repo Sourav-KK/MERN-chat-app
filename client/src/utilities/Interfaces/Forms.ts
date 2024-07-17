327900619e17d5483c69111934dd4f6087ef87aa
Sourav <souravunofficial000@gmail.com>
2024-07-17 13:18:27 +0530

    solved sopme merge isssue

--- client/src/utilities/Interfaces/Forms.ts
+++ client/src/utilities/Interfaces/Forms.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-interface ValuesI {
-  name: string;
-=======
 interface SignupFormSchema_I {
   fullName: string;
->>>>>>> 5607caf7a3795fd6618d20f5fe37795eb341a2dd
   email: string;
   userName: string;
   password: string;
@@ -16,8 +11,4 @@ interface LoginValuesI {
   password: string;
 }
 
-<<<<<<< HEAD
-export type { ValuesI, LoginValuesI };
-=======
 export type { SignupFormSchema_I, LoginValuesI };
->>>>>>> 5607caf7a3795fd6618d20f5fe37795eb341a2dd
--- client/src/utilities/validations/SignupValidation.ts
+++ client/src/utilities/validations/SignupValidation.ts
@@ -1,21 +1,21 @@
 import Zod from "zod";
 
-<<<<<<< HEAD
-export const signupFormSchema = Zod.object({
-  name: Zod.string()
-    .min(4, "Minimum 4 characters")
-    .max(16, "Maximum 16 characters"),
-  userName: Zod.string()
-    .min(4, "Minimum 4 characters")
-    .max(16, "Maximum 16 characters"),
-  email: Zod.string()
-    .email()
-    .min(4, "Invalid email")
-    .max(20, "Maximum 20 characters"),
-  gender: Zod.string().min(4, "Select your Gender").max(6, "Select again"),
-  password: Zod.string().min(2, "min length").max(16, "MAx lenth"),
-});
-=======
+// <<<<<<< HEAD
+// export const signupFormSchema = Zod.object({
+//   name: Zod.string()
+//     .min(4, "Minimum 4 characters")
+//     .max(16, "Maximum 16 characters"),
+//   userName: Zod.string()
+//     .min(4, "Minimum 4 characters")
+//     .max(16, "Maximum 16 characters"),
+//   email: Zod.string()
+//     .email()
+//     .min(4, "Invalid email")
+//     .max(20, "Maximum 20 characters"),
+//   gender: Zod.string().min(4, "Select your Gender").max(6, "Select again"),
+//   password: Zod.string().min(2, "min length").max(16, "MAx lenth"),
+// });
+// =======
 export const signupFormValidator = Zod.object({
   fullName: Zod.string({ required_error: "Full name required" })
     .min(4, "Minimum 4 characters")
@@ -35,9 +35,9 @@ export const signupFormValidator = Zod.object({
     .max(16, "Maximum 16 characters"),
 });
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 export type signupFormValidator_I = typeof signupFormValidator
->>>>>>> 5607caf7a3795fd6618d20f5fe37795eb341a2dd
-=======
-export type signupFormValidator_I = typeof signupFormValidator;
->>>>>>> client
+// >>>>>>> 5607caf7a3795fd6618d20f5fe37795eb341a2dd
+// =======
+// export type signupFormValidator_I = typeof signupFormValidator;
+// >>>>>>> client
